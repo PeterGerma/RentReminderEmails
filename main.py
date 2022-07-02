28356d9ebf4d183c3c1c071fa3953b3bf1ef4362
pgerma <peter.germa@gmail.com>
2022-07-02 14:36:16 -0500

    Initial Code

---
+++ main.py
@@ -0,0 +1,18 @@
+# This Python script will send reminder e-mails to tenants who have oustanding balances on apartments.com to avoid a late fee
+
+import datetime as dt
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
